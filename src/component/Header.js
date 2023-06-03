ed1774683854d5992db958e4da878ace41546f39
AnnaLartey <anna.t.lartey@gmail.com>
2023-06-03 18:36:20 +0000

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -15,12 +15,12 @@ function Header() {
   return (
     <div>
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav className="flex items-center justify-between p-6 pt-0 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="/#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
-                className="h-8 w-auto"
+                className="h-20 w-auto"
                 src={logo}
                 alt=""
               />
