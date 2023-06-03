f63baceee5758051dab2ea2ad14b1acae43e6785
AnnaLartey <anna.t.lartey@gmail.com>
2023-06-03 17:03:35 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import About from './component/About';
 import Faqs from './component/Faqs';
 import Footer from './component/Footer';
+import Header from './component/Header';
 import Home from './component/Home';
 import WhyUs from './component/WhyUs';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Home/>
       <About/>
       <WhyUs/>
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -174,7 +174,8 @@ function Footer() {
             </div>
           </div>
           <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
-            <p className="text-xs leading-5 text-gray-400">&copy; 2020 Your Company, Inc. All rights reserved.</p>
+            <p className="text-xs leading-5 text-gray-400">&copy; 2023 Carefinder, Inc. All rights reserved.</p>
+            <p className="text-xs leading-5 text-gray-400">Created by Anna Lartey</p>
           </div>
         </div>
       </footer>
