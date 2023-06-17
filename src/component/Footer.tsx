32b000ae62afa8520cab26dea14f3c183355f415
AnnaLartey <anna.t.lartey@gmail.com>
2023-06-17 06:46:04 +0000

    Update Footer.tsx

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -119,7 +119,7 @@ function Footer() {
             <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
               <div className="md:grid md:grid-cols-2 md:gap-8">
                 <div>
-                  <h3 className="text-sm font-semibold leading-6 text-gray-500">Solutions</h3>
+                  <h3 className="text-sm font-semibold leading-6 text-blue-500">Solutions</h3>
                   <ul className="mt-6 space-y-4">
                     {footerNavigation.solutions.map((item) => (
                       <li key={item.name}>
@@ -131,7 +131,7 @@ function Footer() {
                   </ul>
                 </div>
                 <div className="mt-10 md:mt-0">
-                  <h3 className="text-sm font-semibold leading-6 text-gray-500">Support</h3>
+                  <h3 className="text-sm font-semibold leading-6 text-blue-500">Support</h3>
                   <ul className="mt-6 space-y-4">
                     {footerNavigation.support.map((item) => (
                       <li key={item.name}>
@@ -145,7 +145,7 @@ function Footer() {
               </div>
               <div className="md:grid md:grid-cols-2 md:gap-8">
                 <div>
-                  <h3 className="text-sm font-semibold leading-6 text-gray-500">Company</h3>
+                  <h3 className="text-sm font-semibold leading-6 text-blue-500">Company</h3>
                   <ul className="mt-6 space-y-4">
                     {footerNavigation.company.map((item) => (
                       <li key={item.name}>
@@ -157,7 +157,7 @@ function Footer() {
                   </ul>
                 </div>
                 <div className="mt-10 md:mt-0">
-                  <h3 className="text-sm font-semibold leading-6 text-gray-500">Legal</h3>
+                  <h3 className="text-sm font-semibold leading-6 text-blue-500">Legal</h3>
                   <ul className="mt-6 space-y-4">
                     {footerNavigation.legal.map((item) => (
                       <li key={item.name}>
