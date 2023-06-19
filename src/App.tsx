502c16aade65b1cacd9a1464933c845b4d5abfaa
AnnaLartey <anna.t.lartey@gmail.com>
2023-06-19 21:41:20 +0000

    Disabled eslint and fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import LandingPage from './component/LandingPage';
-import SignIn from './component/SignIn'
-import SignUp from './component/Signup'
-import HospitalSearch from './component/HospitalSearch';
-import UserAuthContext from './context/UserAuthContext';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./component/LandingPage";
+import SignIn from "./component/SignIn";
+import SignUp from "./component/Signup";
+import HospitalSearch from "./component/HospitalSearch";
+import UserAuthContext from "./context/UserAuthContext";
 
 function App() {
   return (
     <UserAuthContext>
       <div className="App">
         <Routes>
-          <Route path="/" element = { <LandingPage/> } />
-          <Route path='signin' element = { <SignIn/> } />
-          <Route path='signup' element = { <SignUp/> } />
-          <Route path='hospital-search' element = { <HospitalSearch/> } />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="hospital-search" element={<HospitalSearch />} />
         </Routes>
       </div>
     </UserAuthContext>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx,tsx}",
-],
+
+export default {
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-  ],
-}
-
+  plugins: [],
+};
